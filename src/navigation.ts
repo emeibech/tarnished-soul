9b9ad26a9c07e6a105804ea9092dc9c1b41bb82d
emeibech <98073805+emeibech@users.noreply.github.com>
2024-09-29 19:11:23 +0800

    Modify icons and logos

--- src/navigation.ts
+++ src/navigation.ts
@@ -28,7 +28,6 @@ export const headerData = {
     //   href: getPermalink('/contact'),
     // },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
@@ -37,6 +36,10 @@ export const footerData = {
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    {
+      ariaLabel: 'Github',
+      icon: 'tabler:brand-github',
+      href: 'https://github.com/onwidget/astrowind',
+    },
   ],
 };
